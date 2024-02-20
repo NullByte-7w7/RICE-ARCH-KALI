6cab1729fb93971a59d703f6fce85d3dd033b14d
DARKSEC <121623691+DARKSECshell@users.noreply.github.com>
2024-02-20 17:36:55 -0300

    install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@
 # VARIABLE
 user=$(who i am | cut -d " " -f1)
 
+
 	#install dependencies
 	echo -n "Installing the dependencies"
 	for i in {1..3}; do
@@ -21,13 +22,13 @@ user=$(who i am | cut -d " " -f1)
 	clear
 
 	#moved i3 for /etc
-	cp -r i3 /etc/
+	sudo cp -r i3 /etc/
 
 	#moved polybar for /home/user/.config
 	cp -r polybar /home/$user/.config/	
 
 	#moved .feh for /home/user/.config
-	chmod +x feh && cp feh /home/$user/.config/.feh && chown $user:$user /home/$user/.config/.feh
+	chmod +x feh && cp feh /home/$user/.config/.feh && chown $user:$user $HOME/.config/.feh
 
 	#moved rofi for /home/user/.config
 	cp -r rofi /home/$user/.config/	
@@ -42,7 +43,12 @@ user=$(who i am | cut -d " " -f1)
 	chmod +x /home/$user/.config/polybar/launch.sh	
 	
 	# RESTART I3 AND INIT POLYBAR
-	sudo i3 restart
-	sudo /home/$user/.config/polybar/launch.sh
+	i3 restart
+	/home/$user/.config/polybar/launch.sh
 
+	#clear screen
+	clear
+
+	sleep 1
+	
 	echo "THANKS FOR USE MY RICE!"
