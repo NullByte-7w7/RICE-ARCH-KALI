f76553344f67053c1ed37290a46914dbaeb21a3a
NullByte <121623691+NullByte-7w7@users.noreply.github.com>
2024-08-08 12:22:42 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,7 +19,6 @@ check(){
 }
 
 # Change shell
-
 change_shell(){
 	path="/usr/bin/chsh"
 
@@ -41,57 +40,57 @@ if [ "$(whoami)" = "root" ]; then
 
 else
 
-	#install dependencies
+	# install dependencies
 	echo -n "Installing the dependencies"
 	for i in {1..3}; do
 	echo -n "."
 	sleep 0.5;
 	done
 
-	#clear
+	# clear
 	/usr/bin/clear
 		
-	#installing
+	# installing
     	sudo pacman -S xorg-xrandr i3 feh fish polybar rofi alacritty picom --needed --noconfirm
 
-	#clear screen
+	# clear screen
 	/usr/bin/clear
 
-	#moved i3 for /etc
+	# moved i3 for /etc
 	sudo /usr/bin/cp -r i3 /etc/
 
-	#moved polybar for /home/user/.config
+	# moved polybar for /home/user/.config
 	/usr/bin/cp -r polybar /home/$user/.config/	
 
-	#moved .feh for /home/user/.config
+	# moved .feh for /home/user/.config
 	/usr/bin/chmod +x feh && cp feh /home/$user/.config/.feh && chown $user:$user /home/$user/.config/.feh
 
-	#moved rofi for /home/user/.config
+	# moved rofi for /home/user/.config
 	/usr/bin/cp -r rofi /home/$user/.config/	
 
-	#moved directory wallpaper for /home/user/.config
+	# moved directory wallpaper for /home/user/.config
 	/usr/bin/cp -r wallpaper /home/$user/.config/
 
-	#moved alacritty for /home/user/.config/
+	# moved alacritty for /home/user/.config/
 	/usr/bin/cp -r alacritty /home/$user/.config/
 
-	#Moved file for /etc/xdg/picom.conf
+	# Moved file for /etc/xdg/picom.conf
 	/usr/bin/cp picom.conf /etc/xdg/picom.conf
 
-	#permissions directory scripts
+	# permissions directory scripts
 	/usr/bin/chown $user:$user /home/darksec/.config/polybar/script
  	/usr/bin/chown $user:$user /home/darksec/.config/polybar/script/*.sh
 
-	#zsh config file
+	# zsh config file
  	/usr/bin/mv .zshrc /home/$user/
   	/usr/bin/mv zshrc.pre-oh-my-zsh /home/$user/.zshrc.pre-oh-my-zsh
 	change_shell
  
-     	#install fonts
+     	# install fonts
     	/usr/bin/cp -r fonts/*ttf /usr/share/fonts/
     	check
 
-	#PERMISSION EXECUTION launch.sh
+	# PERMISSION EXECUTION launch.sh
 	/usr/bin/chmod +x /home/$user/.config/polybar/launch.sh	
     	/usr/bin/chown $user:$user/.config/polybar/launch.sh
 
@@ -99,7 +98,7 @@ else
 	i3 restart
 	/home/$user/.config/polybar/launch.sh
 
-	#clear screen
+	# clear screen
 	/usr/bin/clear
 
 	/usr/bin/sleep 1
