021b7cfcb731a8336a23da1390f1f3a5064a2b5b
NullByte <121623691+NullByte-7w7@users.noreply.github.com>
2025-06-17 20:13:08 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 #HI, DEVELOPER NULLBYTE, THIS IS MY RICE FOR ARCH LINUX.
 
 # VARIABLE
-user=$(who i am | cut -d " " -f1)
-
+echo -n "[*] NAME YOUR ACCOUNT: "
+read $user
 
 # check executable exist
 check(){
@@ -11,7 +11,7 @@ check(){
 
     if [ -e "$path" ]; then
         echo "[*] Found '$path'"
-	/usr/bin/fc-cache -fv
+        /usr/bin/fc-cache -fv
     else
         /usr/bin/echo "[!] NOT FOUND $path. PLEASE INSTALL fc-cache"
         exit 1
@@ -20,14 +20,14 @@ check(){
 
 # Change shell
 change_shell(){
-	path="/usr/bin/chsh"
+        path="/usr/bin/chsh"
 
-  	if [ -e "$path" ]; then 
-  		/usr/bin/chsh -s /usr/bin/zsh
-	else
- 		/usr/bin/echo "[!] NOT FOUND PATH $path PLEASE INSTALL chsh"
-		exit 1
-	fi
+        if [ -e "$path" ]; then 
+                /usr/bin/chsh -s /usr/bin/zsh
+        else
+                /usr/bin/echo "[!] NOT FOUND PATH $path PLEASE INSTALL chsh"
+                exit 1
+        fi
 }
 
 
@@ -40,69 +40,69 @@ if [ "$(whoami)" = "root" ]; then
 
 else
 
-	# install dependencies
-	echo -n "Installing the dependencies"
-	for i in {1..3}; do
-	echo -n "."
-	sleep 0.5;
-	done
+        # install dependencies
+        echo -n "Installing the dependencies"
+        for i in {1..3}; do
+        echo -n "."
+        sleep 0.5;
+        done
 
-	# clear
-	/usr/bin/clear
-		
-	# installing
-    	sudo pacman -S xorg-xrandr i3 feh fish polybar rofi alacritty picom --needed --noconfirm
+        # clear
+        /usr/bin/clear
 
-	# clear screen
-	/usr/bin/clear
+        # installing
+        sudo apt install xorg-xrandr i3 feh fish polybar rofi alacritty picom
 
-	# moved i3 for /etc
-	sudo /usr/bin/cp -r i3 /etc/
+        # clear screen
+        /usr/bin/clear
 
-	# moved polybar for /home/user/.config
-	/usr/bin/cp -r polybar /home/$user/.config/	
+        # moved i3 for /etc
+        sudo /usr/bin/cp -r i3 /etc/
 
-	# moved .feh for /home/user/.config
-	/usr/bin/chmod +x feh && cp feh /home/$user/.config/.feh && chown $user:$user /home/$user/.config/.feh
+        # moved polybar for /home/user/.config
+        /usr/bin/cp -r polybar /home/$user/.config/
 
-	# moved rofi for /home/user/.config
-	/usr/bin/cp -r rofi /home/$user/.config/	
+        # moved .feh for /home/user/.config
+        /usr/bin/chmod +x feh && cp feh /home/$user/.config/.feh && chown $user:$user /home/$user/.config/.feh
 
-	# moved directory wallpaper for /home/user/.config
-	/usr/bin/cp -r wallpaper /home/$user/.config/
+        # moved rofi for /home/user/.config
+        /usr/bin/cp -r /usr/share/rofi /home/$user/.config/
 
-	# moved alacritty for /home/user/.config/
-	/usr/bin/cp -r alacritty /home/$user/.config/
+        # moved directory wallpaper for /home/user/.config
+        /usr/bin/cp -r ./wallpaper /home/$user/.config/
 
-	# Moved file for /etc/xdg/picom.conf
-	/usr/bin/cp picom.conf /etc/xdg/picom.conf
+        # moved alacritty for /home/user/.config/
+        /usr/bin/cp -r ./alacritty /home/$user/.config/
 
-	# permissions directory scripts
-	/usr/bin/chown $user:$user /home/darksec/.config/polybar/script
- 	/usr/bin/chown $user:$user /home/darksec/.config/polybar/script/*.sh
+        # Moved file for /etc/xdg/picom.conf
+        sudo /usr/bin/cp ./picom.conf /etc/xdg/picom.conf
 
-	# zsh config file
- 	/usr/bin/mv .zshrc /home/$user/
-  	/usr/bin/mv zshrc.pre-oh-my-zsh /home/$user/.zshrc.pre-oh-my-zsh
-	change_shell
+        # permissions directory scripts
+        /usr/bin/chown $user:$user /home/$user/.config/polybar/script
+        /usr/bin/chown $user:$user /home/$user/.config/polybar/script/*.sh
+
+        # zsh config file
+        sudo /usr/bin/cp .zshrc /home/$user/.zshrc
+        sudo /usr/bin/cp -r zshrc.pre-oh-my-zsh /home/$user/.zshrc.pre-oh-my-zsh
+        change_shell
  
-     	# install fonts
-    	/usr/bin/cp -r fonts/*ttf /usr/share/fonts/
-    	check
+        # install fonts
+        /usr/bin/cp -r fonts/*ttf /usr/share/fonts/
+        check
 
-	# PERMISSION EXECUTION launch.sh
-	/usr/bin/chmod +x /home/$user/.config/polybar/launch.sh	
-    	/usr/bin/chown $user:$user/.config/polybar/launch.sh
+        # PERMISSION EXECUTION launch.sh
+        /usr/bin/chmod +x /home/$user/.config/polybar/launch.sh
+        /usr/bin/chown $user:$user/.config/polybar/launch.sh
 
-	# RESTART I3 AND INIT POLYBAR
-	i3 restart
-	/home/$user/.config/polybar/launch.sh
+        # RESTART I3 AND INIT POLYBAR
+        i3 restart
+        /home/$user/.config/polybar/launch.sh
 
-	# clear screen
-	/usr/bin/clear
+        # clear screen
+        /usr/bin/clear
 
-	/usr/bin/sleep 1
-	
-	/usr/bin/echo "THANKS FOR USE MY RICE! RESTART COMPUTER OR NOTEBOOK AND SELECT I3"
+        /usr/bin/sleep 1
+
+        /usr/bin/echo "THANKS FOR USE MY RICE! RESTART COMPUTER OR NOTEBOOK AND SELECT I3"
 
 fi
